347fe9e23b2e5b1da8012161a70c4b3433274fe3
LegendDawon <41173953+DawonChoi@users.noreply.github.com>
2019-10-23 15:28:09 +0900

    Update sinewaveSongGenerator.py

--- advancedWork/sinewaveSongGenerator.py
+++ advancedWork/sinewaveSongGenerator.py
@@ -6,6 +6,7 @@ import numpy as np
 p = pyaudio.PyAudio()
 tuple_duration = [int(i) for i in input().split()]
 
+PI = np.pi
 volume = 0.5     # range [0.0, 1.0]
 fs = 44100       # sampling rate, Hz, must be integer
 duration = 3.0   # in seconds, may be float
@@ -32,18 +33,18 @@ def generateSong(song, durationlist):
     flagOfTempo = 0
     settingDuration(song)
     for symbol in song:
-        samples = (np.sin(2*np.pi*np.arange(fs*durationlist[flagOfTempo])*symbol/fs)).astype(np.float32)
+        samples = (np.sin(2*PI*np.arange(fs*durationlist[flagOfTempo])*symbol/fs)).astype(np.float32)
         flagOfTempo = flagOfTempo + 1
         stream.write(volume*samples)
         time.sleep(0.05)
         print(samples) # drivers
 
 def generateSineWave(frequency):
-    samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+    samples = (np.sin(2*PI*np.arange(fs*duration)*f/fs)).astype(np.float32)
     while frequency >= 30:
         stream.write(volume*samples)
         frequency = frequency - gap
-        samples = (np.sin(2*np.pi*np.arange(fs*duration)*frequency/fs)).astype(np.float32)
+        samples = (np.sin(2*PI*np.arange(fs*duration)*frequency/fs)).astype(np.float32)
         print(samples) # drivers
 
 ###############################################################################################
@@ -52,7 +53,7 @@ def generateSineWave(frequency):
 ###############################################################################################
 
 # generate samples, note conversion to float32 array
-samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+samples = (np.sin(2*PI*np.arange(fs*duration)*f/fs)).astype(np.float32)
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paFloat32,
